3d539c8d12a95483ea8fe804793315ae7484dfeb
RenandRosa <142436623+RenandRosa@users.noreply.github.com>
2023-08-23 17:24:52 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,115 @@
+/*let a = 3;
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+
+console.log(a % 2);
+console.log(b % 2);
+console.log(c % 2);
+console.log(d % 2);
+console.log(e % 2);
+*/
+
+//minha idade
+/*let minhaIdade = 15
+console.log("Antes", minhaIdade);
+minhaIdade = minhaIdade + 1;
+console.log("Depois", minhaIdade);
+*/
+
+/*
+//Exercicio 1
+const a = 3;
+const b = 4;
+console.log(a + b);
+
+const d = 5;
+console.log(3 * 5 / 2);
+
+console.log((d - b) * - 1);
+
+const g = 234
+console.log(g % 5);
+*/
+
+//Exercicio 2
+/*
+const a = 1
+const b = 2
+const c = 3
+console.log(a === a);
+console.log(a === b);
+
+console.log(a !== b);
+console.log(a !== a);
+
+console.log(b > a);
+console.log(b > c);
+
+console.log(b < c);
+console.log(b < a);
+*/
+
+//Comparadores logicos
+/*
+let a = 5;
+let b = 3;
+let c = 0;
+
+console.log(a === b || a !== c);
+console.log(a === b && a !== c);
+console.log("---- separador ----");
+
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+*/
+
+//Exercicio 3
+/*
+let a = true
+let b = false
+let c = true
+
+console.log(a && b);
+console.log(b && c);
+console.log(a && c);
+console.log(a && b && c);
+*/
+
+//Exercicio 4
+/*
+let a = true
+let b = false
+let c = true
+
+console.log(a || b);
+console.log(b || c);
+console.log(a || c);
+console.log(a || b || c);
+*/
+
+//Negação
+/*
+let a = true;
+let b = false;
+
+console.log(a);
+console.log(!a);
+
+console.log(b);
+console.log(!b);
+*/
+
+//Exercicio 5
+let nome = "Renan";
+let idade = 15;
+let idadeFutura = idade + 27;
+
+console.log(nome);
+console.log(idade);
+console.log(idade >=18);
+console.log(idadeFutura);
+
+console.log("Nome:", nome, "Idade:", idade, "É maior de idade?", idade >=18, "Idade em 2050:", idadeFutura);
